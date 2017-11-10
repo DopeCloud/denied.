f75db5c10d8753a96c7dbc7354a9fe0873d64741
ƬHE ЯAW ☣ <raw@dopehosting.net>
2017-11-10 15:04:46 +0000

    Update country.sh

--- country.sh
+++ country.sh
@@ -1,10 +1,25 @@
 #china.
+route add -net 47.92.0.0/15 gw 127.0.0.1 lo
 route add -net 58.242.0.0/15 gw 127.0.0.1 lo
+route add -net 60.168.0.0/13 gw 127.0.0.1 lo
+route add -net 103.207.36.0/22 gw 127.0.0.1 lo
+route add -net 118.69.0.0/20 gw 127.0.0.1 lo
 route add -net 123.51.168.0/24 gw 127.0.0.1 lo
+route add -net 123.207.56.0/23 gw 127.0.0.1 lo
 route add -net 123.244.0.0/14 gw 127.0.0.1 lo
+route add -net 	180.160.0.0/13 gw 127.0.0.1 lo
 
 #Croatia
 route add -net 5.188.10.0/24 gw 127.0.0.1 lo
+
+#india
+route add -net 52.160.0.0/11 gw 127.0.0.1 lo
+route add -net 59.90.208.0/20 gw 127.0.0.1 lo
+route add -net 202.164.53.0/24 gw 127.0.0.1 lo
+route add -net 	117.196.96.0/20 gw 127.0.0.1 lo
+
+#Argentina
+route add -net 190.48.0.0/16 gw 127.0.0.1 lo
 
 #Malaysia
 route add -net 101.99.64.0/21 gw 127.0.0.1 lo
@@ -12,5 +27,42 @@ route add -net 101.99.64.0/21 gw 127.0.0.1 lo
 #UK
 route add -net 95.149.0.0/16 gw 127.0.0.1 lo
 
+#US
+route add -net 50.93.176.0/20 gw 127.0.0.1 lo
+
 #Korea
 route add -net 14.56.0.0/13 gw 127.0.0.1 lo
+route add -net 61.72.0.0/13 gw 127.0.0.1 lo
+route add -net 112.160.0.0/13 gw 127.0.0.1 lo
+route add -net 222.96.0.0/13 gw 127.0.0.1 lo
+
+#Thailand
+route add -net 183.89.144.0/21 gw 127.0.0.1 lo
+
+#Poland
+route add -net 155.133.38.0/24 gw 127.0.0.1 lo
+route add -net 155.133.64.0/24 gw 127.0.0.1 lo
+route add -net 155.133.82.0/24 gw 127.0.0.1 lo
+route add -net 156.67.106.0/24 gw 127.0.0.1 lo
+route add -net 156.67.90.0/24 gw 127.0.0.1 lo
+route add -net 185.38.248.0/22 gw 127.0.0.1 lo
+route add -net 188.68.224.0/20 gw 127.0.0.1 lo
+route add -net 188.68.240.0/20 gw 127.0.0.1 lo
+route add -net 193.42.147.0/24 gw 127.0.0.1 lo
+route add -net 195.22.124.0/24 gw 127.0.0.1 lo
+route add -net 195.22.125.0/24 gw 127.0.0.1 lo
+route add -net 195.22.126.0/23 gw 127.0.0.1 lo
+route add -net 46.29.16.0/21 gw 127.0.0.1 lo
+route add -net 46.29.18.19/32 gw 127.0.0.1 lo
+route add -net 85.190.241.0/24 gw 127.0.0.1 lo
+route add -net 91.229.212.0/24 gw 127.0.0.1 lo
+route add -net 91.234.217.0/24 gw 127.0.0.1 lo
+
+#CA
+route add -net 52.224.0.0/11 gw 127.0.0.1 lo
+
+#RO
+route add -net 79.112.0.0/13 gw 127.0.0.1 lo
+
+#MX
+route add -net 200.77.240.0/20 gw 127.0.0.1 lo
