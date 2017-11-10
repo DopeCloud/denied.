a085cb6bdb9aff97b6d0e09cd030a85d10164d01
ƬHE ЯAW ☣ <raw@dopehosting.net>
2017-11-10 14:49:46 +0000

    Update country.sh

--- country.sh
+++ country.sh
@@ -1,6 +1,16 @@
 #china.
-route add -net 123.51.168.0/24 gw 127.0.0.1 lo
 route add -net 58.242.0.0/15 gw 127.0.0.1 lo
+route add -net 123.51.168.0/24 gw 127.0.0.1 lo
+route add -net 123.244.0.0/14 gw 127.0.0.1 lo
 
 #Croatia
-route add -net 	5.188.10.0/24 gw 127.0.0.1 lo
+route add -net 5.188.10.0/24 gw 127.0.0.1 lo
+
+#Malaysia
+route add -net 101.99.64.0/21 gw 127.0.0.1 lo
+
+#UK
+route add -net 95.149.0.0/16 gw 127.0.0.1 lo
+
+#Korea
+route add -net 14.56.0.0/13 gw 127.0.0.1 lo
