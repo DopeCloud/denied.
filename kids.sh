d7a6928c869addf987b74751d0e2a8a54ed8dbb5
ƬHE ЯAW ☣ <raw@dopehosting.net>
2017-11-11 04:21:19 +0100

    Update kids.sh

--- kids.sh
+++ kids.sh
@@ -6,6 +6,24 @@ route add -net 173.48.0.0/12    gw 127.0.0.1 lo
 route add -net 178.137.128.0/20 gw 127.0.0.1 lo
 route add -net 200.97.243.0/24  gw 127.0.0.1 lo
 route add -net 212.83.160.0/19  gw 127.0.0.1 lo
-
+route add -net 93.188.32.0/21   gw 127.0.0.1 lo
+route add -net 104.144.104.0/21 gw 127.0.0.1 lo
+route add -net 104.144.224.0/19 gw 127.0.0.1 lo
+route add -net 104.160.244.0/24 gw 127.0.0.1 lo
+route add -net 107.152.156.0/22 gw 127.0.0.1 lo
+route add -net 107.172.96.0/21  gw 127.0.0.1 lo
+route add -net 155.94.192.0/20  gw 127.0.0.1 lo
+route add -net 158.222.0.0/20   gw 127.0.0.1 lo
+route add -net 162.244.8.0/21   gw 127.0.0.1 lo
+route add -net 185.158.132.0/22 gw 127.0.0.1 lo
+route add -net 191.101.64.0/20  gw 127.0.0.1 lo
+route add -net 191.101.96.0/20  gw 127.0.0.1 lo
+route add -net 192.3.240.0/21   gw 127.0.0.1 lo
+route add -net 193.192.58.0/23  gw 127.0.0.1 lo
+route add -net 193.201.224.0/22 gw 127.0.0.1 lo
+route add -net 196.247.17.0/24  gw 127.0.0.1 lo
+route add -net 196.247.17.0/24  gw 127.0.0.1 lo
+route add -net 196.247.17.0/24  gw 127.0.0.1 lo
+route add -net 198.52.128.0/17  gw 127.0.0.1 lo
 
 ip route add blackhole 103.197.33.82
